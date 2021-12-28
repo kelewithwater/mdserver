0bdf4472aba1df17cd0bae2577085834d0787eaa
jiale <3177408592@qq.com>
2021-12-28 20:31:59 +0800

    更新了异步更新内容

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
 import os
+import json
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    a = os.listdir(r'doc')
-    return render_template('index.html',a = a)
+    with open('url.json', 'r', encoding = 'utf-8') as f:
+        a = json.load(f)
+    return render_template('index.html')
+
+@app.route('/url')
+def url():
+    with open('url.json', 'r', encoding = 'utf-8') as f:
+        a = json.load(f)
+        return json.dumps(a)
 
 app.run(debug=True)
